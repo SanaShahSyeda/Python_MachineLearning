d653d48fe0ecb8451273fa105c33ca98a906dd04
SanaShahSyeda <132450783+SanaShahSyeda@users.noreply.github.com>
2023-08-26 21:58:58 -0700

    Basic variables intro

---
+++ Python_Variables_builtinFunctions.py
@@ -0,0 +1,31 @@
+
+carname = "Audi"
+print(carname)
+x = 30
+print(x)
+x=5
+y=15
+z=x+y
+print(z)
+myfirst_name="john"
+print(myfirst_name)
+a = b = c =1
+print(c)
+localVar= "hello"
+def display():
+    localVar="world"
+    print(localVar)
+display()
+print(localVar) 
+
+
+# In[47]:
+s="This is a university"
+print(len(s))
+print(s.replace("university", "college"))
+print(s.upper())
+print(s[len(s)-2])
+print(s[-2])
+print(s.strip())
+print(len(s.split(" ")))
+
